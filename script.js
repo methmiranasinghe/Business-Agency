6710f2afd5d9c38c18e5be6ce6aba99f0058b4da
methmiranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2021-12-03 00:03:57 +0530

    nav bar

---
+++ script.js
@@ -0,0 +1,12 @@
+//navbar 
+const menu=document.querySelector('.menu')
+const navbar =document.querySelector('.navbar')
+
+menu.addEventListener('click',()=>{
+    navbar.classList.toggle('change')
+    menu.classList.toggle('change')
+})
+
+
+
+//End of Nav bar
