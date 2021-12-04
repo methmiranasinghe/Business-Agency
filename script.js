0489cc92e8e3c89e0bc6d3682ce2061c7479bf5b
methmiranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2021-12-04 11:32:15 +0530

    about us section video controls

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-//navbar 
-const menu=document.querySelector('.menu')
-const navbar =document.querySelector('.navbar')
+// Navbar
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
 
-menu.addEventListener('click',()=>{
-    navbar.classList.toggle('change')
-    menu.classList.toggle('change')
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle('change')
+});
+// End of Navbar
+
+// Section 2 Video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons button i');
+const bar = document.querySelector('.video-bar')
+
+const playPause = () => {
+  if(video.paused) {
+    video.play()
+    btn.className = 'far fa-pause-circle'
+    video.style.opacity = '.7'
+  } else {
+    video.pause()
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  }
+}
+
+btn.addEventListener('click', () => {
+  playPause()
 })
 
 
-
-//End of Nav bar
+video.addEventListener('timeupdate', () => {
+  console.log(video.currentTime, video.duration)
+  const barWidth = video.currentTime / video.duration;
+  bar.style.width = `${barWidth * 100}%` 
+  if(video.ended) {
+    btn.className = 'far fa-play-circle'
+    video.style.opacity = '.3'
+  } 
+})
+// End of Section 2 Video
